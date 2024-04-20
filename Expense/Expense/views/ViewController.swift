2cdd4dd1d8056c747d5511bd93bc6847a4336282
aiexxash <ialexballa@gmail.com>
2024-04-20 14:02:03 +0300

    feat: Connect storyboard view with UIViewController

--- Expense/Expense/views/ViewController.swift
+++ Expense/Expense/views/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var totalBalanceButton: UIButton!
+    @IBOutlet weak var todayExpenseButton: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var requestButton: UIButton!
+    @IBOutlet weak var recentTransictionTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func totalBalanceButtonClicked(_ sender: UIButton){}
+    @IBAction func todayExpenseButtonClicked(_ sender: UIButton){}
+    @IBAction func sendButtonClicked(_ sender: UIButton){}
+    @IBAction func requestButtonClicked(_ sender: UIButton){}
 }
 
